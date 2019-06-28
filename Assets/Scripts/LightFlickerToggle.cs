cc38af6c325360a0cf1ae108f15b03fb510e6526
PixelDough <alworrell19@gmail.com>
2019-06-28 01:04:38 -0500

    Added TriggerVolume and LightFlickerToggle. Worked on Dining Hall room.

---
+++ Assets/Scripts/LightFlickerToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlickerToggle : MonoBehaviour
+{
+    public bool targetState;
+    public Light lightToEffect;
+    public TriggerVolume triggerVolume;
+    public bool autoReset = false;
+
+    bool m_IsActive = false;
+    float m_flickerTime = 1f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerVolume.isTriggered)
+        {
+            m_IsActive = true;
+        }
+
+        if (m_IsActive)
+        {
+            if (m_flickerTime < 60f)
+            {
+                lightToEffect.gameObject.SetActive( (m_flickerTime % 10 < 5) ^ targetState );
+            }
+            else
+            {
+                lightToEffect.gameObject.SetActive( false ^ targetState );
+            }
+
+            m_flickerTime++;
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerVolume.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerVolume : MonoBehaviour
+{
+    public GameObject targetObject;
+    public bool toggleWhenInside = true;
+
+    public bool isTriggered = false;
+
+    private void Update()
+    {
+        if (targetObject == null)
+        {
+            isTriggered = (toggleWhenInside ? false : true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == targetObject.name)
+        {
+            if (toggleWhenInside)
+            {
+                isTriggered = true;
+            }
+            else
+            {
+                isTriggered = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == targetObject.name)
+        {
+            if (!toggleWhenInside)
+            {
+                isTriggered = true;
+            }
+            else
+            {
+                isTriggered = false;
+            }
+        }
+    }
+}
